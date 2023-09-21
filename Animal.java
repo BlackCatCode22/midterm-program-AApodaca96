5bf26bbe9ffa0d6d23c9c6c9213f2480c0e99992
Angelo Greg Apodaca <110855579+AApodaca96@users.noreply.github.com>
2023-09-21 14:24:11 -0700

    Add files via upload
    Consolidated Main.java code & Added Hyena.java class

--- Animal.java
+++ Animal.java
@@ -1,14 +1,31 @@
 public class Animal {
 
+    // Counter
+    public static int numOfAnimals = 0;
+
+
+    // Constructor
+    public Animal() {
+        numOfAnimals++;
+    }
+
+
+    // Fields/Attributes
+    String name;
     String desc;
-
     String birthSeason;
-
     String color;
-
     String weight;
-
     String origin01;
-
     String origin02;
+
+
+    // Methods
+    public void makenoise() {
+        System.out.println("This is an animal object making noise... ");
+    }
+    public String makenoise02() {
+        return "This is a string returned from a method in my Animal Class";
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,87 +1,33 @@
-import java.util.ArrayList;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
 
 public class Main {
     public static void main(String[] args) {
 
         // Welcome Statement
-        System.out.printf("\nHello and welcome to Angelo's Class/Midterm Review!\n\n");
+        System.out.printf("\nHello and welcome to Angelo's Zoo Program!\n\n");
 
-        // Create an Array
-        ArrayList<Animal> animalList = new ArrayList<>();
+        // Create Animal object
+        Animal myNewAnimal = new Animal();
 
-        // Define the File I/O path
-        String filePath = "C:/Users/angel/IdeaProjects/CIT_63_Java/Classes_Review_Sept_19th_2023/src/animalNames.txt";
+        myNewAnimal.name = "Leo";
+        System.out.println("\n the name of the new animal is: " + myNewAnimal.name);
 
-        try {
-            // Create FileReader and BufferedReader objects
-            FileReader fileReader = new FileReader(filePath);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+        // Hyenas
+        Hyena myHyena = new Hyena();
+        Hyena myHyena02 = new Hyena();
+        Hyena myHyena03 = new Hyena();
 
-            // Read lines from the file
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+        // How many Hyenas do we have?
+        System.out.println("We have " + Hyena.numOfHyenas + " Hyenas!");
 
-               /* // Mohle's Code
-                String[] myArray = line.split(",");
-                System.out.println("\n myArray[0] is " + myArray[0]);
-                System.out.println("\n myArray[1] is " + myArray[1]);*/
+        myHyena.name = "Zig";
 
-                // Create an Animal object from the Animal Class
-                Animal anyOldAnimal = new Animal();
+        System.out.println("\n My new Hyena is named: " + myHyena.name);
 
-                // Fill the object's data fields
-               /* anyOldAnimal.desc = myArray[0];
-                anyOldAnimal.birthSeason = myArray[1];
-                anyOldAnimal.color = myArray[2];
-                anyOldAnimal.weight = myArray[3];
-                anyOldAnimal.origin01 = myArray[4];
-                anyOldAnimal.origin02 = myArray[5];
-*/
-                // Add the newly created animal object to the array list
+        myHyena.makenoise();
 
+        System.out.println("He makes this noise: " + myHyena.makenoise02());
 
-            }
-
-            // Close the BufferedReader and FileReader
-            bufferedReader.close();
-            fileReader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-
-
-        //myNewAnimal.desc = " this is my desc";
-
-       // System.out.println("\n the desc of the new animal is: " + myNewAnimal.desc);
-
-        // Open the arriving animal text file (File I/O)
-
-
-
-
-
-
-
-    /*
-        // Use a for loop to create five Animal objects and add them to the ArrayList
-        for (int i = 1, i <= 5; i++) {
-            // Using the constructor with arguments
-            Animal animal = new Animal("Animal" + i);
-            AnimalList.add(animal);
-        }
-
-        // Output the animalList with an item in loop
-        for (Animal animal : animalList) {
-            System.out.println(animal.name + " " + animal.age);
-        }
-    */
-
+        // How many animals do we have
+        System.out.println("\n After using new twice, we have " + Animal.numOfAnimals + " animals in the zoo!\n");
     }
 }
